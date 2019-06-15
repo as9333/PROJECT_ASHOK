9546849e90129c00165a38656d5a5d18b4f60b0d
Ashok Manoj Philip <ashokmanojphilip@gmail.com>
2019-06-15 19:49:33 +0530

    initial commit
    the ip and details are currently working in iptest.php

---
+++ iptest.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+//$details = json_decode(file_get_contents("http://ipinfo.io/111.92.72.39/json"));
+//echo $details->city;
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+//DEBUG PURPOSE
+foreach($details as $x => $x_value) 
+{
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "<br>";
+}
+?>
+</body>
+</html>

b7e7b58f5134f42ee7ff78636d6ad2921ab03954
Ashok Manoj Philip <ashokmanojphilip@gmail.com>
2019-06-24 01:22:00 +0530

    fully ip address kandupidikan eppol patum
    ip address kitiyal penne db local nennum location kitunathe working
    
    atyavesham nalla accurassy
    oru test ui kodi undakkitunde search_db.php

---
+++ search_db.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+	<form method="POST" name="na">
+	<b>Who do you what to search for:</b>
+	<br>
+	<input type="text" name="search">
+	<input type="submit" value="SEARCH" name="submit">
+	</form>
+	
+</body>
+
+<?php
+if(isset($_POST['submit']))
+	{
+		require_once('db_connect.php');
+		require_once('ip_from_db.php');
+		$search = $_POST['search'];
+		echo "<br>";
+		echo "Searched for $search";
+		//$query = "SELECT
+	}
+?>

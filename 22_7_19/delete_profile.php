ae309b679e581d223dcbea290ee2a1f5ffbe85db
as9333 <ashokmanojphilip@gmail.com>
2019-07-22 15:14:52 +0530

    code update

---
+++ 22_7_19/delete_profile.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once('connect.php');
+
+$uname = $_SESSION['username'];
+$pass = $_SESSION['user_pass'];
+// echo $uname;
+// echo $pass;
+$query = "DELETE FROM `users` WHERE username='$uname' AND password='$pass'";
+$retrn = mysqli_query($con,$query);
+
+if($retrn==1)
+    {
+        echo '<script type="text/javascript">';
+		echo 'alert("Profile Deleted Successfully")';
+		echo '</script>';
+    session_unset(); 
+    session_destroy();
+        header("Location: {$home_url}index.php");
+		exit(); 
+    }
+  else 
+  	{	
+  		echo '<script type="text/javascript">';
+		echo 'alert("No Profile Found")';
+		echo '</script>';
+        header("Location: {$home_url}index.php");
+		exit(); 
+  	}
+?>
---
+++ 22_7_19/not_loggedin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>YOU ARE NOT LOGGED IN </title>
+</head>
+<body>
+<H1>YOU ARE NOT LOGGED IN</H1>
+<H1>YOU WILL BE REDIRECTED </H1>
+<script>setTimeout(function () { window.location.href= 'index.php'; },5000);</script>
+</body>
+</html>
